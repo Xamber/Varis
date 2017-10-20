e16537ec7318fb6e4721c9d37a6c0a19fc402333
Artem Filippov <Artem.Filippov@emc.com>
2017-10-20 11:19:50 +0300

    Delete unused utils functions

--- utils.go
+++ utils.go
@@ -2,36 +2,18 @@ package varis
 
 import "math"
 
-// Функция вычисляет среднее от среза
-func mean(data []float64) float64 {
-	total := 0.0
-	for _, value := range data {
-		total += value
-	}
-
-	return total / float64(len(data))
-}
-
-// Функция активации (Сигмойда)
+// Activation function (sigmoid)
 func activation_sigmoid(x float64) float64 {
 	return 1 / (1 + math.Exp(-x))
 }
 
-// Производная от функции активации
+// Derivative function
 func derivative_sigmoid(x float64) float64 {
 	var fx = activation_sigmoid(x)
 	return fx * (1 - fx)
 }
 
-// Инструмент выполнения функции заданное количество раз
-func repeat(f func(), times int) {
-	for times > 0 {
-		f()
-		times--
-	}
-}
-
-// Инструмент подсчета суммы элементов функции
+// Function for sum all elements in slice
 func sum(data []float64) float64 {
 	var result float64
 	for _, i := range data {
