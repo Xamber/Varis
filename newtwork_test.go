bd95e4f468fd87dc58067f825cbf843cf1100f73
Artem Filippov <Artem.Filippov@emc.com>
2017-10-23 17:06:07 +0300

    Change my example function to test benchmark

---
+++ newtwork_test.go
@@ -0,0 +1,29 @@
+package varis
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkNetwork(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		n := CreateNetwork(2, 3, 1)
+
+		dataset := Dataset{
+			Frame{[]float64{0.0, 0.0}, []float64{1.0}},
+			Frame{[]float64{1.0, 0.0}, []float64{0.0}},
+			Frame{[]float64{0.0, 1.0}, []float64{0.0}},
+			Frame{[]float64{1.0, 1.0}, []float64{1.0}},
+		}
+
+		trainer := Trainer{&n, BackPropagation}
+		trainer.TrainByDataset(dataset, 10000)
+
+		fmt.Println("After training")
+		fmt.Println(0.0, 0.0, "-", n.Calculate(0.0, 0.0))
+		fmt.Println(1.0, 0.0, "-", n.Calculate(1.0, 0.0))
+		fmt.Println(0.0, 1.0, "-", n.Calculate(0.0, 1.0))
+		fmt.Println(1.0, 1.0, "-", n.Calculate(1.0, 1.0))
+	}
+}
