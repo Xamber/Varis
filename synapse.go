a82e90d222898ff672b675da372d88405b09c468
Artem Filippov <Artem.Filippov@emc.com>
2017-10-24 18:40:51 +0300

    Refactoring

--- synapse.go
+++ synapse.go
@@ -5,31 +5,29 @@ import (
 )
 
 type synapse struct {
-	weight    float64
-	in        chan float64
-	out       chan float64
-	inNeuron  Neuron
-	outNeuron Neuron
-	cache     float64
+	weight float64
+	in     chan float64
+	out    chan float64
+	cache  float64
+}
+
+func (syn *synapse) Live() {
+	for {
+		syn.cache = <-syn.in
+		outputValue := syn.cache * syn.weight
+		syn.out <- outputValue
+	}
 }
 
 func createSynapse(in Neuron, out Neuron) {
 	syn := &synapse{
-		weight:    rand.Float64(),
-		in:        make(chan float64),
-		out:       make(chan float64),
-		inNeuron:  in,
-		outNeuron: out,
+		weight: rand.Float64(),
+		in:     make(chan float64),
+		out:    make(chan float64),
 	}
 
 	in.getConnection().addOutputSynapse(syn)
 	out.getConnection().addInputSynapse(syn)
 
-	go func() {
-		for {
-			syn.cache = <-syn.in
-			outputValue := syn.cache * syn.weight
-			syn.out <- outputValue
-		}
-	}()
+	go syn.Live()
 }
