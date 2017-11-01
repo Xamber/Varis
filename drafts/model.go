d66843e5f60b39ec18f01ef9fe8c81b8a546d168
Artem Filippov <Artem.Filippov@emc.com>
2017-11-01 10:48:24 +0300

    Update tests

--- drafts/model.go
+++ drafts/model.go
@@ -1,4 +1,4 @@
-package varis
+package drafts
 //
 //import "fmt"
 //
--- tests/varis_test.go
+++ tests/varis_test.go
@@ -1,23 +1,24 @@
-package varis
+package varis_tests
 
 import (
 	"fmt"
 	"testing"
+	"github.com/xamber/Varis"
 )
 
 func BenchmarkNetwork(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
-		n := CreateNetwork(2, 3, 1)
+		n := varis.CreateNetwork(2, 3, 1)
 
-		dataset := Dataset{
+		dataset := varis.Dataset{
 			{[]float64{0.0, 0.0}, []float64{1.0}},
 			{[]float64{1.0, 0.0}, []float64{0.0}},
 			{[]float64{0.0, 1.0}, []float64{0.0}},
 			{[]float64{1.0, 1.0}, []float64{1.0}},
 		}
 
-		BackPropagation(&n, dataset, 10000)
+		varis.BackPropagation(&n, dataset, 10000)
 
 		fmt.Println("After training")
 		fmt.Println(0.0, 0.0, "-", n.Calculate(0.0, 0.0))
