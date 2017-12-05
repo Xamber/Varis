92f94e0ea42f4f8c98f79ed154ddcc6676457d30
Artem Filippov <Artem.Filippov@emc.com>
2017-12-05 17:54:00 +0300

    Remove benchmarks

--- varis_test.go
+++ varis_test.go
@@ -1,11 +1,11 @@
 package varis
 
 import (
-	"fmt"
+	"math/rand"
 	"testing"
 )
 
-func TestSum(t *testing.T) {
+func TestVectorSum(t *testing.T) {
 	vector := Vector{1.1, 2.2, 3.3}
 	summa := vector.sum()
 	if summa != 6.6 {
@@ -13,7 +13,7 @@ func TestSum(t *testing.T) {
 	}
 }
 
-func TestZeroSum(t *testing.T) {
+func TestVectorZeroSum(t *testing.T) {
 	vector := Vector{}
 	summa := vector.sum()
 	if summa != 0.0 {
@@ -28,8 +28,59 @@ func TestGenerateUUID(t *testing.T) {
 	}
 }
 
-func TestCreation(t *testing.T) {
+func TestNetworkFunction(t *testing.T) {
 	_ = CreateNetwork(2, 3, 1)
+}
+
+func TestDeactivationFunction(t *testing.T) {
+	if DEACTIVATION(0.123123123) != 0.24905493220237876 {
+		t.Error("Wrong deactivation function")
+	}
+}
+
+func TestActivationFunction(t *testing.T) {
+	if ACTIVATION(0.123123123) != 0.5307419550064931 {
+		t.Error("Wrong activation function")
+	}
+}
+
+func TestNetworkTrain(t *testing.T) {
+	nn := CreateNetwork(2, 3, 1)
+
+	dataset := Dataset{
+		{Vector{0.0, 0.0}, Vector{1.0}},
+		{Vector{1.0, 0.0}, Vector{0.0}},
+		{Vector{0.0, 1.0}, Vector{0.0}},
+		{Vector{1.0, 1.0}, Vector{1.0}},
+	}
+
+	BackPropagation(&nn, dataset, 10000)
+
+	if nn.Calculate(Vector{0.0, 0.0})[0] < 0.800000 {
+		t.Log("Wrong with calculation: ", Vector{0.0, 0.0}, " result: ", nn.Calculate(Vector{0.0, 0.0})[0])
+	}
+
+	if nn.Calculate(Vector{1.0, 0.0})[0] > 0.200000 {
+		t.Log("Wrong with calculation: ", Vector{1.0, 0.0}, " result: ", nn.Calculate(Vector{1.0, 0.0})[0])
+	}
+
+	if nn.Calculate(Vector{0.0, 1.0})[0] > 0.200000 {
+		t.Log("Wrong with calculation: ", Vector{0.0, 1.0}, " result: ", nn.Calculate(Vector{0.0, 1.0})[0])
+	}
+
+	if nn.Calculate(Vector{1.0, 1.0})[0] < 0.800000 {
+		t.Log("Wrong with calculation: ", Vector{1.0, 1.0}, " result: ", nn.Calculate(Vector{1.0, 1.0})[0])
+	}
+
+}
+
+func TestNetworkCalculate(t *testing.T) {
+	rand.Seed(1488)
+	nn := CreateNetwork(2, 3, 1)
+
+	if nn.Calculate(Vector{0.0, 0.0})[0] != 0.6631222149019123 {
+		t.Error("Wrong with calculation")
+	}
 }
 
 func TestDumpToJSON(t *testing.T) {
